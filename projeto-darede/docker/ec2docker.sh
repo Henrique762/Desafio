7615922f60a5746c60306cbb6e06f82d6ac0e3a0
Henrique <henriquegil123@gmail.com>
2022-11-05 17:34:39 -0300

    variosbagulhomsm

--- projeto-darede/docker/ec2docker.sh
+++ projeto-darede/docker/ec2docker.sh
@@ -4,9 +4,9 @@ sudo apt upgrade -y
 sudo apt install git -y
 git clone https://github.com/Henrique762/Desafio.git
 curl -fsSL https://get.docker.com -o get-docker.sh
-DRY_RUN=1 sudo sh ./get-docker.sh
-cd /Desafio/Projeto-Darede/Docker 
+DRY_RUN=1 sudo sh ./get-docker.sh 
 sudo docker swarm init
+cd /Desafio/Projeto-Darede/Docker
 sudo docker stack deploy -c docker-compose.yaml sites
 
 
