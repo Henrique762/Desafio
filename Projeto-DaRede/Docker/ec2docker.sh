5d8eeae6384763e23c6ee16809aef1d20be0f5dd
Henrique <henriquegil123@gmail.com>
2022-11-05 18:34:41 -0300

    krl

---
+++ Projeto-DaRede/Docker/ec2docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git -y
+git clone https://github.com/Henrique762/Desafio.git
+curl -fsSL https://get.docker.com -o get-docker.sh
+DRY_RUN=1 sudo sh ./get-docker.sh 
+sudo docker swarm init
+cd /Desafio/projeto-Darede/docker/docker-compose/
+sudo docker stack deploy -c docker-compose.yaml sites
+
+
