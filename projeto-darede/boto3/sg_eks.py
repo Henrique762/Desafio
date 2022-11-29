807402b5b4642a290053a6cf7b8eeafbcd31ea13
Henrique <henriquegil123@gmail.com>
2022-11-29 20:05:09 -0300

    adicionando o python

--- projeto-darede/boto3/sg_eks.py
+++ projeto-darede/boto3/sg_eks.py
@@ -1,12 +1,63 @@
 import boto3
-from botocore.exceptions import ClientError
 
+open_rules = '0.0.0.0/0'
+
+exFromPort = 'FromPort'
+exToPort = 'ToPort'
+region = 'us-east-2'
+
+
+sns_client = boto3.client('sns', region_name='us-east-2')
+
+def publish_message():
+    response = sns_client.publish(
+        TopicArn='arn:aws:sns:us-east-2:613036180535:h-topic',
+        Message="SG com ID (sg-059050c0d9c51566a) ESTA EXPOSTO PARA O MUNDO",
+        Subject="SG DO CLUSTER",
+        )
+
+def publish_message_sg():
+    response = sns_client.publish(
+        TopicArn='arn:aws:sns:us-east-2:613036180535:h-topic',
+        Message="SG COM ID (sg-0c800cd973474591c) ESTA EXPOSTO PARA O MUNDO",
+        Subject="SG DO CLUSTER",
+        )
+
+
+client = boto3.client('ec2', region_name='us-east-2')
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name='us-east-1')
+    response = client.describe_security_groups(GroupIds=['sg-059050c0d9c51566a'])
 
-    try:
-        response = ec2.describe_security_groups(GroupIds=['sg-02d27ddb102f7336c'])
-        print(f'CIDR permissions {ec2.ip-permission.cidr}')
-    
-    except ClientError as e:
-        print(e)
+    for sg in response["SecurityGroups"]:
+        # grab these variables so that they're sent to the revoke security group access
+        groupId = sg['GroupId']
+        groupName = sg['GroupName']
+
+        for ip in sg['IpPermissions']:
+            # there has to be an exception for security groups that don't have ports.
+            if exFromPort in ip:
+                fromPort = ip['FromPort']
+                ipProtocol = ip['IpProtocol']
+                toPort = ip['ToPort']
+                for cidr in ip['IpRanges']:
+                    # identifying which rules contain a open_rules IP range
+                    if cidr['CidrIp'] == open_rules:
+                        publish_message()
+
+    sg2 = client.describe_security_groups(GroupIds=['sg-0c800cd973474591c'])
+
+    for sg in sg2["SecurityGroups"]:
+        # grab these variables so that they're sent to the revoke security group access
+        groupId = sg['GroupId']
+        groupName = sg['GroupName']
+
+        for ip in sg['IpPermissions']:
+            # there has to be an exception for security groups that don't have ports.
+            if exFromPort in ip:
+                fromPort = ip['FromPort']
+                ipProtocol = ip['IpProtocol']
+                toPort = ip['ToPort']
+                for cidr in ip['IpRanges']:
+                    # identifying which rules contain a open_rules IP range
+                    if cidr['CidrIp'] == open_rules:
+                        publish_message_sg()
